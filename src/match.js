32ec379ec4e5948e395c5104aea34a7ad101a523
h41ku <h41ku.c0de@ya.ru>
2025-03-13 14:35:30 +0300

    Use helper function

--- src/match.js
+++ src/match.js
@@ -1,9 +1,8 @@
+import detailsFromUrl from './detailsFromUrl.js'
+
 export default (pattern, url) => {
     const result = url.pathname.match(pattern)
     if (!result)
         return; // undefined
-    const { search, hash } = url
-    const params = { ...result.groups }
-    const query = Object.fromEntries(new URLSearchParams(search))
-    return { params, query, hash }
+    return detailsFromUrl(url, { ...result.groups })
 }
