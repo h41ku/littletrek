f6db56df3115fadf6bb0441106d783a5e17b4c83
h41ku <h41ku.c0de@ya.ru>
2025-03-13 14:48:08 +0300

    Update router's API
    - Add method `fallback` to define a fallback-handler;
    - Add roadmap for each HTTP-method;
    - Add support for instance of `Request` as argument of method `resolve`;
    - Add `request` into details for handlers;
    - Replace method `add` with methods named like lowercased HTTP-methods;
    - Update return type of handlers;
    - Update types;

--- src/createRouter.js
+++ src/createRouter.js
@@ -1,21 +1,50 @@
 import createPattern from './createPattern.js'
 import match from './match.js'
+import detailsFromUrl from './detailsFromUrl.js'
 
 export default () => {
-    const roadmap = new Map()
+    const maps = new Map()
+    const getMap = method => {
+        let map = maps.get(method)
+        if (!map) {
+            map = new Map()
+            maps.set(method, map)
+        }
+        return map
+    }
     let router
-    const add = (pattern, action) => {
-        roadmap.set(createPattern(pattern), { pattern, action })
+    let fallback = () => {}
+    const roadmap = (method) => (pattern, action) => {
+        getMap(method).set(createPattern(pattern), { pattern, action })
         return router
     }
-    const resolve = (url, base) => { // `base` is undefined by default
-        if (!(url instanceof URL))
-            url = new URL(url, base)
-        for (const [ regexp, { action } ] of roadmap) {
-            const request = match(regexp, url)
-            if (request)
-                return action(request)
+    const resolve = (request, origin) => { // `origin` is undefined by default
+        if (!(request instanceof Request))
+            request = new Request(
+                request instanceof URL
+                    ? request
+                    : new URL(request, origin)
+            )
+        const { method, url: href } = request
+        const url = new URL(href)
+        for (const [ regexp, { action } ] of getMap(method)) {
+            const details = match(regexp, url)
+            if (details)
+                return action({ ...details, request })
         }
+        return fallback({ ...detailsFromUrl(url), request })
     }
-    return (router = { add, resolve })
+    return (router = {
+        get: roadmap('GET'),
+        put: roadmap('PUT'),
+        post: roadmap('POST'),
+        delete: roadmap('DELETE'),
+        patch: roadmap('PATCH'),
+        head: roadmap('HEAD'),
+        connect: roadmap('CONNECT'),
+        options: roadmap('OPTIONS'),
+        trace: roadmap('TRACE'),
+        resolve,
+        fallback: handler => { fallback = handler }
+    })
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,29 @@
-export type Request = { params: object, query: object, search: string, hash: string };
-export type RequestHandler = (request: Request) => void;
-export type Router = {
-    add(pattern: string, handler: RequestHandler): Router,
-    resolve(location: string | URL, origin: string | undefined): void
+export type MatchDetails = {
+    params: object,
+    query: object,
+    search: string,
+    hash: string,
+    request: Request
 };
+
+export type Handler = (details: MatchDetails) => any | undefined;
+
+export type Router = {
+
+    get(pattern: string, handler: Handler): Router,
+    put(pattern: string, handler: Handler): Router,
+    post(pattern: string, handler: Handler): Router,
+    patch(pattern: string, handler: Handler): Router,
+    delete(pattern: string, handler: Handler): Router,
+    head(pattern: string, handler: Handler): Router,
+    connect(pattern: string, handler: Handler): Router,
+    options(pattern: string, handler: Handler): Router,
+    trace(pattern: string, handler: Handler): Router,
+
+    resolve(request: Request | URL | string, origin: string | undefined): any | undefined,
+    fallback(handler: Handler): any | undefined
+};
+
 export type RouterConnection = {
     disconnect(): void,
     navigate(url: string): void,
